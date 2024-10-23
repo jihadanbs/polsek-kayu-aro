5b8089607d3c16927ea8c9c06ca3ab855e3a9994
jihadanbs <jihadanbs11@gmail.com>
2024-10-23 11:54:33 +0700

    update model TbDesa

--- app/Models/DesaModel.php
+++ app/Models/DesaModel.php
@@ -9,7 +9,7 @@ class DesaModel extends Model
     protected $table = 'tb_desa';
     protected $primaryKey = 'id_desa';
     protected $retunType = 'object';
-    protected $allowedFields = ['id_user', 'nama_desa', 'kecamatan', 'kabupaten', 'provinsi', 'kode_pos', 'luas_wilayah', 'jumlah_penduduk', 'website'];
+    protected $allowedFields = ['id_user', 'nama_desa', 'kecamatan', 'kabupaten', 'provinsi', 'kode_pos', 'luas_wilayah', 'jumlah_penduduk', 'website', 'jumlah_kepala_keluarga', 'jumlah_penduduk_pria', 'jumlah_penduduk_wanita', 'jumlah_penduduk_usia_0_14', 'jumlah_penduduk_usia_15_64', 'jumlah_penduduk_usia_65_keatas', 'jumlah_penduduk_tidak_sekolah', 'jumlah_penduduk_sd', 'jumlah_penduduk_smp', 'jumlah_penduduk_sma_smk', 'jumlah_penduduk_diploma_sarjana', 'jumlah_penduduk_bekerja', 'jumlah_penduduk_tidak_bekerja', 'jumlah_penduduk_belum_menikah', 'jumlah_penduduk_menikah', 'jumlah_penduduk_cerai', 'jumlah_sekolah', 'jumlah_posyandu', 'jumlah_tempat_ibadah', 'jumlah_pos_ronda'];
     protected $useTimestamps = true;
     protected $useSoftDeletes = false;
 
